3b82fb745ebd03d5a6dc75c2b0cc84b44f56985a
Pieter Van Keymeulen <pvankeymeulen@seanachaidh.be>
2017-12-19 22:52:04 +0100

    Eerst commit

---
+++ classifiers.py
@@ -0,0 +1,7 @@
+#We are not going to build our own classifiers but we are going to
+#create a framework so that we can easily plug them in trough a common model
+
+class Classifier:
+    def __init__(self, name, ):
+        pass
+    
