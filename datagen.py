991e417b8e080bc13e07f7802f815127529e83e7
Pieter Van Keymeulen <pvankeymeulen@seanachaidh.be>
2017-12-21 23:34:29 +0100

    bomen uitproberen

--- datagen.py
+++ datagen.py
@@ -3,8 +3,8 @@ from random import randint
 def generate_testdata(rows, cols, outfile):
     with open(outfile, 'w') as f:
         for _ in range (rows):
-            toprint = [randint(0,1)] * cols
-            print(f, ','.join(toprint))
+            toprint = [str(randint(0,1)) for _ in range(cols)]
+            f.write(','.join(toprint) + '\n')
 
 def f():
     return 1
--- tree.py
+++ tree.py
@@ -1,17 +1,32 @@
 from scipy.cluster import hierarchy
+from matplotlib import pyplot as plt
 import numpy as np
 import pandas
+
+# What should this tree be capeble of doing?
+# 1. Be non-binary
+#   That is: When datapoints are of equal distance, it should be both a child of its parent
+#   Can be emulated: Scan the tree for brances with same length and copy the children under the same node
 
 class Tree:
     
     def load_from_file(self,filename):
         dnamatrix = pandas.read_csv(filename,header=None)
         print(dnamatrix)
-        np.append(self.data, dnamatrix, axis=0)
+        self.data = np.append(self.data, dnamatrix, axis=0)
+        
+    def gen_tree(self):
+        self.tree = hierarchy.linkage(self.data, 'ward')
+        
+    def plot_tree(self):
+        plt.figure()
+        plt.title('testtree')
+        hierarchy.dendrogram(self.tree, truncate_mode='lastp', p=3)
+        plt.show()
     
     def __init__(self):
-        self.data = np.array([])
+        self.data = np.empty((0,20), float)
         self.tree = []
     def __str__(self):
-        pass
+        return str(self.data)
     
