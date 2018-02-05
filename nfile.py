207da8174f03772c00cc6e816b2fd9792bd6dbb9
Pieter Van Keymeulen <pvankeymeulen@seanachaidh.be>
2018-02-05 23:32:57 +0100

    Tokens worden getaget

--- nfile.py
+++ nfile.py
@@ -2,7 +2,9 @@ import nltk
 from bz2 import BZ2File
 
 def only_word(tokens):
-	
+	#Nog eens nakijken of ik moet specifiëren of het start of endigt met een onderliggend streepje
+    return [x.trim('_') for x in tokens]
+
 
 class NidhoggFile:
 	def __init__(self, filename):
@@ -13,4 +15,6 @@ class NidhoggFile:
 			
 		#Then we create the tokens
 		self.tokens = nltk.word_tokenize(self.data)
+		self.striped_tokens = only_word(self.tokens)
+		self.postag = nltk.pos_tag(self.stripped_tokens
 		
