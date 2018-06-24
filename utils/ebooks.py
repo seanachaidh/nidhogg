25aaf93d4cd26fdd3e711e47ad7316739119be5e
Pieter Van Keymeulen <pvankeymeulen@seanachaidh.be>
2018-06-24 16:42:38 +0200

    add ebooks imlementation

---
+++ utils/ebooks.py
@@ -0,0 +1,15 @@
+import os
+from xml.etree import ElementTree
+
+class TOC:
+    def __init__(self, filename):
+        pass
+
+class EPUB:
+    def __init__(self, author, title):
+        pass
+    
+    @staticmethod
+    def load_from_file(filename):
+        return None
+    
